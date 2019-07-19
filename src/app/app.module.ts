b3a087a3a215a96d8ca0d98d965459048efab125
Spawer <spaweradrian@gmail.com>
2019-07-19 10:12:31 +0200

    Creation component questionnaire questionnaire-form questionnaire-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { QuestionnaireFormComponent } from './questionnaire-form/questionnaire-form.component';
+import { QuestionnaireListComponent } from './questionnaire-list/questionnaire-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponent,
+    QuestionnaireFormComponent,
+    QuestionnaireListComponent
   ],
   imports: [
     BrowserModule,
